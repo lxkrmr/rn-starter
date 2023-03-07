e3a8541bf58b118b757758bb483599410b84a5aa
Alex <lxkrmr@icloud.com>
2023-03-07 15:06:33 +0100

    ✨ add Box Demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,6 +15,7 @@ const HomeScreen = ({ navigation: { navigate } }) => {
       <Button onPress={() => navigate('Color')} title="Go to Color Demo" />
       <Button onPress={() => navigate('Square')} title="Go to Square Demo" />
       <Button onPress={() => navigate('Text')} title="Go to Text Demo" />
+      <Button onPress={() => navigate('Box')} title="Go to Box Demo" />
     </View>
   );
 };
